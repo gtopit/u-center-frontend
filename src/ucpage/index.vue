5731799375327b2c0de200208fe2749dd8b5c767
hongzw <hongzw@citicloud.com.cn>
2023-01-09 13:57:02 +0800

    前端代码初始化

---
+++ src/ucpage/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <navbar/>
+    </div>
+</template>
+
+<script>
+    import Navbar from '@/ucpage/Navbar'
+    export default {
+        components: {
+            Navbar
+        }
+    }
+</script>
